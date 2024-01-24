c46ea20f584a0059cb16f36a21d232f022eb776f
FujiiWill <fujiiwiliam@gmail.com>
2024-01-24 16:43:54 -0300

    Drill finished

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./routes/Home";
+import HomeBody from "./routes/Home/HomeBody";
+import Sales from "./routes/Home/Sales";
+import Subscription from "./routes/Subscription";
 
 function App() {
-  
   return (
-    
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}>
+          <Route index element={<HomeBody />} />
+          <Route path="sales" element={<Sales />} />
+        </Route>
+        <Route path="/subscription" element={<Subscription />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
